89bcd00cfedcd5889d5cd868fdec8d5a1074206d
Stone Zhang <stnzhl@gmail.com>
2019-05-20 06:25:17 +0900

    Create initial framework project

---
+++ BookbinderCocoapodsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BookbinderCocoapodsDemo
+//
+//  Created by Stone Zhang on 5/19/19.
+//  Copyright © 2019 Stone Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
