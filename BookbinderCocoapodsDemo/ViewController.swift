98c57e4eb9183230afb0f0473cf67fc53f092994
Stone Zhang <stnzhl@gmail.com>
2019-05-20 23:08:32 +0900

    Add Bookbinder 1.0.0 dependency via Cocoapods

--- BookbinderCocoapodsDemo/ViewController.swift
+++ BookbinderCocoapodsDemo/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import Bookbinder
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let bookbinder = Bookbinder()
+        print(bookbinder.configuration.rootURL)
     }
 
 
---
+++ Pods/Target Support Files/Bookbinder/Bookbinder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Bookbinder : NSObject
+@end
+@implementation PodsDummy_Bookbinder
+@end
